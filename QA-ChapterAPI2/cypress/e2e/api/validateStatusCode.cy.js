fb6899b3be02515f424dec5c3c2cf200cd149492
emejajeonduty <emjeajeonduty@gmail.com>
2024-08-05 16:27:25 +0800

    update file validateStatusCode.cy.js

---
+++ QA-ChapterAPI2/cypress/e2e/api/validateStatusCode.cy.js
@@ -0,0 +1,21 @@
+describe('Validate Headers Response', () => {
+    //cara 1
+    it('coba validate status code', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon');
+        cy.get('@pokemon').its('status').should('equal',200)
+    });
+
+    //cara 2
+    it.only('coba validate status code', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://pokeapi.co/api/v2/pokemon/ditto'
+        }).as('pokemon');
+
+        cy.get('@pokemon').then((response) => {
+            expect(response.status).equal(200);
+        })
+    });
+    it
+
+});
